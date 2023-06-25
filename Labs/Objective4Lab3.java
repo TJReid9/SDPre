77d1b1bffa0dd063357d9e2d78b6faa3707fd5df
TJReid9 <tjreid20@icloud.com>
2023-06-25 14:12:11 -0600

    Objective4.Lab3.java

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.time.Year;
+
+public class Objective4Lab3 {
+	public static void main(String[] args) {
+		int birthYear, age;
+
+		birthYear = 0;
+
+
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("How old are you?");
+			age = keyboard.nextInt();
+			keyboard.close();
+
+		int currentYear = Year.now() .getValue();
+
+		birthYear = currentYear - age;
+		
+
+
+		System.out.println("You were born in " + birthYear);
+		keyboard.close();
+	}
+}
