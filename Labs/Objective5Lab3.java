9f9c720b8c838c410f73d855fd852ac550aad76a
TJReid9 <tjreid20@icloud.com>
2023-06-25 22:52:28 -0600

    Completed Objective5Lab3.java

---
+++ Labs/Objective5Lab3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Objective5Lab3 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int userNum;
+		int num1 = 0;
+
+		System.out.println("Please enter a number: ");
+		userNum = scanner.nextInt();
+
+		if (userNum < num1) {
+			System.out.println("The number is negative.");
+		}
+		else if (userNum > num1) {
+			System.out.println("The number is positive.");
+		}
+		else if (userNum == num1) {
+			System.out.println("The number is equal to num1.");
+		}
+	}
+}
