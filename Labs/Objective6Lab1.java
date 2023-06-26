0c39b91bbec4a16b4368edbd419b3b48e751d96b
TJReid9 <tjreid20@icloud.com>
2023-06-26 16:48:45 -0600

    Completed Objective6Lab1.java

---
+++ Labs/Objective6Lab1.java
@@ -0,0 +1,35 @@
+public class Objective6Lab1 {
+	public static void main(String[] args) {
+		int month = 6;
+
+		switch (month) {
+		case 1:
+			System.out.println("January");
+			break;
+
+		case 2:
+			System.out.println("Febuary");
+			break;
+
+		case 3:
+			System.out.println("March");
+			break;
+
+		case 4:
+			System.out.println("April");
+			break;
+
+		case 5:
+			System.out.println("May");
+			break;
+
+		case 6:
+			System.out.println("June");
+			break; 
+
+		default:
+			System.out.println("This is not a calid month");
+			break;
+		}
+	}
+}
