3fc01c8054709763e45f7175aef3b030ae3c5503
TJReid9 <tjreid20@icloud.com>
2023-06-26 21:15:30 -0600

    Completed Objective7Lab4.java

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int count = 20, i = 1, sum = 0;
+
+		while (i <= count) {
+			sum = sum + i;
+			i++;
+		}
+
+		System.out.println(sum);
+	}
+}
