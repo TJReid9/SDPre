7c7689162961a7d7fb439965883813a33108a2df
TJReid9 <tjreid20@icloud.com>
2023-06-27 15:04:59 -0600

    Completed Objective8Lab1.java

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+	public static void main(String[] args) {
+
+		for (int iUp = 1; iUp <= 10; iUp++) {
+			System.out.println(iUp);
+		}
+
+		for (int iDown = 10; iDown >= 1; iDown--) {
+			System.out.println(iDown);
+		}
+	}
+}
