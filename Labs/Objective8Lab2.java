4db26f03f8b94e7c44e6d57b3e497581cc79ee5e
TJReid9 <tjreid20@icloud.com>
2023-06-27 15:34:06 -0600

    Complete Objective8Lab2.java

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int count = 20, sum = 0;
+
+		for (int i = 1; i <= count; ++i) {
+			sum += i;
+		}
+
+		System.out.println(sum);
+	}
+}
