93a0746a57c4ee9d55905aa8f1bd6ed99a0fd997
TJReid9 <tjreid20@icloud.com>
2023-06-26 18:19:32 -0600

    Completed Objective7Lab2.java

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,70 @@
+public class Objective7Lab2 {
+	public static void main(String[] args) {
+		int counter = 0;
+
+		while(counter <= 20) {
+			System.out.println("O");
+			counter = counter + 1;
+
+			System.out.println("1");
+			counter = counter + 1;
+
+			System.out.println("2");
+			counter = counter + 1;
+
+			System.out.println("3");
+			counter = counter + 1;
+
+			System.out.println("4");
+			counter = counter + 1;
+
+			System.out.println("5");
+			counter = counter + 1;
+
+			System.out.println("6");
+			counter = counter + 1;
+
+			System.out.println("7");
+			counter = counter + 1;
+
+			System.out.println("8");
+			counter = counter + 1;
+
+			System.out.println("9");
+			counter = counter + 1;
+
+			System.out.println("10");
+			counter = counter + 1;
+
+			System.out.println("11");
+			counter = counter + 1;
+			
+			System.out.println("12");
+			counter = counter + 1;
+
+			System.out.println("13");
+			counter = counter + 1;
+
+			System.out.println("14");
+			counter = counter + 1;
+
+			System.out.println("15");
+			counter = counter + 1;
+
+			System.out.println("16");
+			counter = counter + 1;
+
+			System.out.println("17");
+			counter = counter + 1;
+
+			System.out.println("18");
+			counter = counter + 1;
+
+			System.out.println("19");
+			counter = counter + 1;
+
+			System.out.println("20");
+			counter = counter + 1;
+		}
+	}
+}
