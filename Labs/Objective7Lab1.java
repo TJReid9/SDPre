22c66efea57015c53d58f58455d7f05de58f9e66
TJReid9 <tjreid20@icloud.com>
2023-06-27 21:41:27 -0600

    Completed Objective7Lab1.java

--- Labs/Objective7Lab1.java
+++ Labs/Objective7Lab1.java
@@ -3,34 +3,7 @@ public class Objective7Lab1 {
 		int counter = 10;
 
 		while(counter > 0) {
-			System.out.println("10");
-			counter = counter - 1;
-
-			System.out.println("9");
-			counter = counter - 1;
-
-			System.out.println("8");
-			counter = counter - 1;
-
-			System.out.println("7");
-			counter = counter - 1;
-
-			System.out.println("6");
-			counter = counter - 1;
-
-			System.out.println("5");
-			counter = counter - 1;
-
-			System.out.println("4");
-			counter = counter - 1;
-
-			System.out.println("3");
-			counter = counter - 1;
-
-			System.out.println("2");
-			counter = counter - 1;
-
-			System.out.println("1");
+			System.out.println(counter);
 			counter = counter - 1;
 
 		}
